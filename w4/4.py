00ad55a03da83ec5f6cf6988fdb4d4914020d5c3
bobur554396 <bobur.muhsimbaev@gmail.com>
2022-02-26 12:49:53 +0600

    asd

--- w4/4.py
+++ w4/4.py
@@ -2,10 +2,38 @@
 
 # a = [1, 2, 3, 4]
 
+from turtle import ycor
+
+
 def gen():
+  print('asdasd')
+  print('asdasd')
+  print('asdasd')
+  print('asdasd')
+  print('asdasd')
   yield 1
+  
+  print('asdasd')
+  print('asdasd')
+  print('asdasd')
+  print('asdasd')
+  print('asdasd')
   yield 2
+  
+  print('asdasd')
+  print('asdasd')
+  print('asdasd')
   yield 3
+
+def users_gen():
+  for i in range(100000):
+    yield i
+
+# users = [1, 2, 3, 4, .... 20bln]
+users = users_gen()
+
+for user in users:
+  print(user)
   
 a = gen()
 # print(a)
---
+++ w4/README.md
@@ -0,0 +1,3 @@
+Lecture examples
+
+https://github.com/askarakshabayev/PP2_2022/blob/main/week4/week4.md
